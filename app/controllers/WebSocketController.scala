8b344100299e61382cc6b3cc443b43d779d4b29f
Michał Sitko <pierwszy1@gmail.com>
2016-08-11 12:40:01 +0200

    workaround with SharedKillSwitch
    see at http://doc.akka.io/docs/akka/2.4.9-RC1/scala/stream/stream-dynamic.html#SharedKillSwitch

--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -3,7 +3,7 @@ package controllers
 import akka.NotUsed
 import akka.actor._
 import akka.pattern.ask
-import akka.stream.Materializer
+import akka.stream._
 import akka.stream.scaladsl.{Flow, Source}
 import akka.util.Timeout
 import play.api.libs.streams.ActorFlow
@@ -15,20 +15,27 @@ import scala.concurrent.duration._
 
 class WebSocketController (implicit serviceA: ActorRef, actorSystem: ActorSystem, materializer: Materializer) extends Controller {
   def index = WebSocket.accept[String, String] { request =>
-    ActorFlow.actorRef(out => MyWebSocketActor.props(serviceA, out))
+    val killSwitch = KillSwitches.shared(scala.util.Random.nextInt.toString)
+    val flow = Flow[String].map(s => s).via(killSwitch.flow)
+    flow.via(ActorFlow.actorRef(out => MyWebSocketActor.props(serviceA, out, killSwitch)))
   }
 }
 
 object MyWebSocketActor {
-  def props(serviceA: ActorRef, out: ActorRef) = Props(new MyWebSocketActor(serviceA, out))
+  def props(serviceA: ActorRef, out: ActorRef, killSwitch: SharedKillSwitch) = Props(new MyWebSocketActor(serviceA, out, killSwitch))
+  case object Stop
 }
 
-class MyWebSocketActor(serviceA: ActorRef, out: ActorRef) extends Actor {
+class MyWebSocketActor(serviceA: ActorRef, out: ActorRef, killSwitch: SharedKillSwitch) extends Actor {
   implicit val timeout: Timeout = 20.seconds
 
   def receive = {
     case msg: String =>
-      out ! "whatever"
+      out ! msg
+      context.system.scheduler.scheduleOnce(300.millis, self, MyWebSocketActor.Stop)
+    case MyWebSocketActor.Stop =>
+      killSwitch.shutdown()
+      println("bazinga 2")
       self ! PoisonPill
   }
 
