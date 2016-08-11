3045ca799703790fdee66a4b32cb5b1d73cd55fc
Michał Sitko <pierwszy1@gmail.com>
2016-08-11 11:47:02 +0200

    probably some bug?

--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -1,20 +1,38 @@
 package controllers
 
-import akka.actor.ActorSystem
+import akka.NotUsed
+import akka.actor._
 import akka.pattern.ask
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Source}
 import akka.util.Timeout
-import play.api.mvc.{Action, Controller}
+import play.api.libs.streams.ActorFlow
+import play.api.mvc.{Controller, WebSocket}
 import services.Service
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-class WebSocketController (actorSystem: ActorSystem) extends Controller {
+class WebSocketController (implicit serviceA: ActorRef, actorSystem: ActorSystem, materializer: Materializer) extends Controller {
+  def index = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef(out => MyWebSocketActor.props(serviceA, out))
+  }
+}
+
+object MyWebSocketActor {
+  def props(serviceA: ActorRef, out: ActorRef) = Props(new MyWebSocketActor(serviceA, out))
+}
+
+class MyWebSocketActor(serviceA: ActorRef, out: ActorRef) extends Actor {
   implicit val timeout: Timeout = 20.seconds
 
-  def index = Action.async {
-    val serviceA = actorSystem.actorOf(Service.props(1200))
-    val response = (serviceA ? Service.Request).mapTo[Int]
-    response.map(r => Ok("hello world" + r))
+  def receive = {
+    case msg: String =>
+      out ! "whatever"
+      self ! PoisonPill
+  }
+
+  override def postStop() = {
+    println("websocket's actor has been stopped")
   }
 }
--- app/general/MyAppLoader.scala
+++ app/general/MyAppLoader.scala
@@ -1,10 +1,12 @@
 package general
 
+import akka.stream.ActorMaterializer
 import controllers.WebSocketController
 import router.Routes
 import play.api.ApplicationLoader.Context
 import play.api.routing.Router
 import play.api.{Application, ApplicationLoader, BuiltInComponentsFromContext}
+import services.Service
 
 class MyAppLoader extends ApplicationLoader {
   override def load(context: Context): Application = new MyAppComponents(context).application
@@ -12,7 +14,8 @@ class MyAppLoader extends ApplicationLoader {
 
 class MyAppComponents(val context: Context) extends BuiltInComponentsFromContext(context) {
   implicit lazy val system = actorSystem
-  val wsController = new WebSocketController(system)
+  val serviceA = actorSystem.actorOf(Service.props(1200))
+  val wsController = new WebSocketController()(serviceA, system, materializer)
 
   override def router: Router = new Routes(httpErrorHandler, wsController)
 }
